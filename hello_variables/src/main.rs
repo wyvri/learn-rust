da000e2bb3fdf9f37b5dbf7139f11d05feb43393
wyvernk <wrennuin@gmail.com>
2024-07-12 17:41:07 -0500

    finish 3.1

--- hello_variables/src/main.rs
+++ hello_variables/src/main.rs
@@ -1,3 +1,21 @@
+const ME : &str = "ren";
+
 fn main() {
-    println!("Hello, world!");
+    let mut x = 5;
+    println!("The value of x is {}", x);
+    x = 6;
+    println!("The value of x is {}", x);
+
+    println!("I am: {}", ME);
+
+    let y = 5;
+
+    let y = y + 1;
+
+    {
+        let y = y * 2;
+        println!("y in the inner scope: {}", y);
+    }
+
+    println!("value of y: {}", y);
 }
