8fdfb4dd0f9278b89ebf94fbb99bdf481edbb347
wyvernk <wrennuin@gmail.com>
2024-07-12 18:36:41 -0500

    make powers of 2 game

---
+++ powers_of_2/src/main.rs
@@ -0,0 +1,34 @@
+use std::io;
+use std::cmp::Ordering;
+
+fn main() {
+    println!("What is the next power of 2?");
+
+    let mut power : u32 = 1;
+    let mut answer = u64::pow(2, power);
+
+    loop {
+        println!("Please type 2 ^ {}:", power);
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u64 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&answer) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => { 
+                println!("You got it!");
+            }
+        };
+        power = power + 1;
+        answer = u64::pow(2, power)
+    }
+}
