731bd3e90ce4379a0b0e97db9f9c34575456250b
wyvernk <wrennuin@gmail.com>
2024-07-12 20:41:21 -0500

    finish functions, on to if statements

--- hello_functions/src/main.rs
+++ hello_functions/src/main.rs
@@ -1,7 +1,21 @@
 fn main() {
     print_labeled_measurement(5, 'h');
+    
+    let x = plus_one(five());
+    println!("The value of x is {x}.")
 }
 
 fn print_labeled_measurement(value: i32, unit_label: char) {
     println!("The measurement is: {value}{unit_label}.");
 }
+
+fn five() -> i32 {
+    5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
+}
+
+// hello comments!
+// and hello if statements
