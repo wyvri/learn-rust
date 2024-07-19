636b25ff0a360f2e1d5e5f057e78adfdf1b0feb7
wyvernk <wrennuin@gmail.com>
2024-07-19 18:54:00 -0500

    add references to hello ownership

--- hello_ownership/src/main.rs
+++ hello_ownership/src/main.rs
@@ -2,12 +2,12 @@ fn main() {
     let my_string = String::from("meow");
     println!("{my_string}");
 
-    let your_string = yum(my_string);
+    let your_string = yum(&my_string);
 
     println!("{your_string}");
 }
 
-fn yum(my_string:String) -> String {
-    let your_string = my_string;
+fn yum(my_string: &String) -> &String {
+    let your_string = &my_string;
     your_string
 }
